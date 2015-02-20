7cc98d0cd797a5e2dc476b27c7caea50c90aa903
Anton Golomazov <agolomazov@itloft.ru>
2015-02-20 16:32:06 +0300

    Add first test download

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -4,8 +4,31 @@ class Main extends CI_Controller {
 
     protected $data = array();
 
+    protected function is_post(){
+
+        return ($_SERVER['REQUEST_METHOD'] == "POST") ? true : false;
+
+    }
+
 
     public function index(){
+
+        if($this->is_post()){
+
+            $files = $this->input->post('phototemplates');
+
+            if(is_array($files)){
+
+                $this->load->library('zip');
+
+                $name = 'photo.jpg';
+                $data = base_url().'img/stickers/sticker1.jpg';
+                $this->zip->add_data($name, $data);
+                $this->zip->download('my_backup.zip');
+
+            }
+
+        }
 
         $this->load->view('main/index');
 
--- application/views/main/index.php
+++ application/views/main/index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 <nav class="navbar navbar-default">
-    <div class="container-fluid">
+    <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -20,25 +20,67 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><img src="http://placehold.it/158x90" alt=""></a>
+            <a class="navbar-brand" href="#"><img src="http://dummyimage.com/158x90" alt=""></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="nav navbar-nav navbar-right topmenu">
                 <li><a href="#">Готовые</a></li>
                 <li><a href="#">Создать свою</a></li>
             </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
+        </div>
+    </div>
 </nav>
 
-<div class="container">
-    <div class="col-xs-8">
-        <h1>Наклейки для айтишников</h1>
-        <h2>Выберите форму наклейки</h2>
-    </div>
+<section id="mainscreen">
+    <div class="container">
+        <div class="col-xs-8 col-xs-offset-2 b-mainscreen">
+            <h1 class="title">Наклейки для айтишников</h1>
+            <h2 class="subtitle">Выберите форму наклейки</h2>
+            <div class="col-xs-6">
+                <img src="./img/type1.png" alt="">
+            </div>
+            <div class="col-xs-6">
+                <img src="./img/type2.png" alt="">
+            </div>
+        </div>
 
+    </div>
+</section>
+
+
+
+<section id="photoline">
+    <div id="spisok">
+        <form method="POST" action="">
+            <ul>
+                <li>
+                    <input type="checkbox" name="phototemplates[]" value="sticker1.jpg" id="photoline-item1">
+                    <label for="photoline-item1"><img src="<?php echo base_url(); ?>img/stickers/sticker1.jpg"></label>
+                </li>
+                <li>
+                    <input type="checkbox" name="phototemplates[]" value="sticker2.jpg" id="photoline-item2">
+                    <label for="photoline-item2"><img src="<?php echo base_url(); ?>img/stickers/sticker2.jpg"></label>
+                </li>
+                <li>
+                    <input type="checkbox" name="phototemplates[]" value="sticker3.jpg" id="photoline-item3">
+                    <label for="photoline-item3"><img src="<?php echo base_url(); ?>img/stickers/sticker3.jpg"></label>
+                </li>
+                <li>
+                    <input type="checkbox" name="phototemplates[]" value="sticker4.jpg" id="photoline-item4">
+                    <label for="photoline-item4"><img src="<?php echo base_url(); ?>img/stickers/sticker4.jpg"></label>
+                </li>
+                <li id="end">
+                    <div></div>
+                </li>
+            </ul>
+            <input type="submit" name="download" value="Заказать">
+        </form>
+    </div>
+</section>
+<div id="footer">
+    <p>&copy;2015 itstickers.ru</p>
 </div>
 
 
